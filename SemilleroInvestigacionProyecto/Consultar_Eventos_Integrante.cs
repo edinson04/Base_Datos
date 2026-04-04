4d2636363d7653f44cfcefc96ca58b6c65ecd211
Carvelasq <cvelasquezc@unicartagena.edu.co>
2026-04-03 19:51:24 -0500

    Update Consultar_Eventos_Integrante.cs

--- SemilleroInvestigacionProyecto/Consultar_Eventos_Integrante.cs
+++ SemilleroInvestigacionProyecto/Consultar_Eventos_Integrante.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,35 @@ namespace SemilleroInvestigacionProyecto
 {
     public partial class Consultar_Evento_Integrante : Form
     {
+        string cedulaInvestigador;
         public Consultar_Evento_Integrante(String idUsuario)
         {
             InitializeComponent();
+            cedulaInvestigador = idUsuario;
+        }
+
+        private void Consultar_Evento_Integrante_Load(object sender, EventArgs e)
+        {
+            Dgv_eventos.DataSource = ObtenerEventosporUsuario(cedulaInvestigador);
+        }
+
+        private DataTable ObtenerEventosporUsuario(string cedulaInvestigador)
+        {
+            DataTable dt = new DataTable();
+            Conexion cn = new Conexion();
+            try 
+            {
+                SqlCommand consulta = new SqlCommand("");
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            finally
+            {
+                cn.Cerrar();
+            }
+            return dt;
         }
     }
 }
